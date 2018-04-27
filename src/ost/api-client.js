a45db7fb468cf0cf45b5a7191870c3fa0708e903
Vliegenthart <daniel.vliegenthart@gmail.com>
2018-04-27 15:58:07 +0900

    Add library and functions to make OST API calls

--- src/ost/api-client.js
+++ src/ost/api-client.js
@@ -2,57 +2,37 @@ const queryString = require('query-string');
 const crypto = require('crypto');
 const request = require('request')
 const config = require("./config")
+const OstKit = require('./ost-kit');
 
+var ok = new OstKit(config.apiKey, config.apiSecret);
 
-
-function generateQueryString(endpoint, inputParams, apiKey, requestTimestamp) {
-  inputParams["api_key"] = apiKey;
-  inputParams["request_timestamp"] = requestTimestamp;
-  const queryParamsString = queryString.stringify(inputParams, {arrayFormat: 'bracket'}).replace(/%20/g, '+');
-  const stringToSign = endpoint + '?' + queryParamsString;
-  return stringToSign;
+function listUsers() {
+  ok.usersList().then((res) => {
+    console.log(res)
+  });
 }
 
-function generateApiSignature(stringToSign, apiSecret) {
-  var buff = new Buffer.from(apiSecret, 'utf8');
-  var hmac = crypto.createHmac('sha256', buff);
-  hmac.update(stringToSign);
-  return hmac.digest('hex');
+function listTransactionTypes() {
+  ok.transactiontypesList().then((res) => {
+    console.log(res)
+  });
 }
 
-
-function createUser() {
-  const baseUrl = "https://playgroundapi.ost.com"
-  const endpoint = "/users/create"
-  const inputParams = { name: "Alice Anderson" }
-  const apiKey = config.apiKey
-  const apiSecret = config.apiSecret
-  const requestTimeStamp = Math.floor(new Date() / 1000)
-
-  const stringToSign = generateQueryString(endpoint, inputParams, apiKey, requestTimeStamp)
-  console.log(stringToSign)
-  const signature = generateApiSignature(stringToSign, apiSecret)
-
-  const query = baseUrl + stringToSign + "&signature=" + signature
-
-  const options = {
-    url: query,
-    headers: {
-      'Accept': "application/json"
-    }
-  }
-
-  function callback(error, response, body) {
-    if (!error && response.statusCode == 200) {
-      var info = JSON.parse(body);
-      console.log(info);
-    }
-    else {
-      console.log(response.statusCode, JSON.parse(body))
-    }
-  }
-
-  request.post(options, callback)
+function createUser(name) {
+  ok.usersCreate({name: name}).then((res) => {
+    console.log(res)
+  }).catch((e) => {
+    console.log("Err", e.response.data.err)
+  });
 }
 
-createUser()
+function rewardUser(to_uuid, transaction_kind) {
+  ok.transactiontypesExecute({from_uuid: config.company_uuid, to_uuid: to_uuid, transaction_kind: transaction_kind}).then((res) => {
+    console.log(res)
+  }).catch((e) => {
+    console.log("Err", e.response.data.err)
+  });
+}
+
+// createUser("John Doe")
+// rewardUser("d086778f-fc7d-4d07-a515-0a90403a4a6d", "Reward")
