cfbe8eda42fa0daca1ee83a700700307891d27b0
Vliegenthart <daniel.vliegenthart@gmail.com>
2018-04-27 22:40:24 +0900

    Add account page with change password option

--- src/components/Account.js
+++ src/components/Account.js
@@ -1,8 +1,18 @@
 import React from 'react';
 
+import AuthUserContext from './AuthUserContext';
+import { PasswordForgetForm } from './PasswordForget';
+import PasswordChangeForm from './PasswordChange';
+
 const AccountPage = () =>
-  <div>
-    <h1>Account Page</h1>
-  </div>
+  <AuthUserContext.Consumer>
+    {authUser =>
+      <div>
+        <h1>Account: {authUser.email}</h1>
+        <PasswordForgetForm />
+        <PasswordChangeForm />
+      </div>
+    }
+  </AuthUserContext.Consumer>
 
 export default AccountPage;
