70bed097629e2166161009e05a954b6c107bac11
Vliegenthart <daniel.vliegenthart@gmail.com>
2018-04-27 16:02:38 +0900

    Add airdrop function wrappers

--- src/ost/api-client.js
+++ src/ost/api-client.js
@@ -34,5 +34,21 @@ function rewardUser(to_uuid, transaction_kind) {
   });
 }
 
+function airdropNewUsers(amount) {
+  ok.usersAirdropDrop({amount: amount, list_type: "never_airdropped"}).then((res) => {
+    console.log(res)
+  }).catch((e) => {
+    console.log("Err", e.response.data.err)
+  });
+}
+
+function airdropAllUsers(amount) {
+  ok.usersAirdropDrop({amount: amount, list_type: "all"}).then((res) => {
+    console.log(res)
+  }).catch((e) => {
+    console.log("Err", e.response.data.err)
+  });
+}
+
 // createUser("John Doe")
 // rewardUser("d086778f-fc7d-4d07-a515-0a90403a4a6d", "Reward")
