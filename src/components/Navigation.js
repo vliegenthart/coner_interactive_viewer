9067449ef9c05cb54ea95cad90cf0c41c2d3aed7
Vliegenthart <daniel.vliegenthart@gmail.com>
2018-04-27 22:33:48 +0900

    Add password reset option

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import AuthUserContext from './AuthUserContext';
 import SignOutButton from './SignOut';
 import * as routes from '../constants/routes';
 
-const Navigation = ({ authUser }) =>
-  <div>
-    { authUser
+const Navigation = () =>
+  <AuthUserContext.Consumer>
+    {authUser => authUser
       ? <NavigationAuth />
       : <NavigationNonAuth />
     }
-  </div>
+  </AuthUserContext.Consumer>
 
 const NavigationAuth = () =>
   <ul>
