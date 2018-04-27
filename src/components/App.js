80ac50ea3efb29e01677aefaa3ba739d8d68340d
Vliegenthart <daniel.vliegenthart@gmail.com>
2018-04-27 20:09:55 +0900

    Add basic navigation, routes and router

--- src/components/App.js
+++ src/components/App.js
@@ -6,233 +6,53 @@
 // TODO
 // - Clean up GitHub epo (remove all highlight files)
 
-import React, { Component } from "react";
+import React from 'react';
+import { 
+  BrowserRouter as Router,
+  Route,
+} from 'react-router-dom';
 
-import URLSearchParams from "url-search-params";
+import Navigation from './Navigation';
+import LandingPage from './Landing';
+import SignUpPage from './SignUp';
+import SignInPage from './SignIn';
+import PasswordForgetPage from './PasswordForget';
+import HomePage from './Home';
+import AccountPage from './Account';
 
-import {
-  PdfLoader,
-  Popup,
-  AreaHighlight
-} from "react-pdf-annotator";
+import * as routes from '../constants/routes';
 
-import PdfAnnotator from "./PDFAnnotator.js";
+const App = () =>
+  <Router>
+    <div>
+      <Navigation />
+      <hr/>
 
-import Tip from "./Tip.js"
-import Highlight from "./Highlight.js"
-
-import termHighlights from "../highlights/conf_trec_BellotCEGL02-highlights";
-
-import Spinner from "./Spinner";
-import Sidebar from "./Sidebar";
-
-import type { T_Highlight, T_NewHighlight } from "react-pdf-annotator/types";
-
-import "../style/App.css";
-
-type T_ManuscriptHighlight = T_Highlight;
-
-type Props = {};
-
-type State = {
-  highlights: Array<T_ManuscriptHighlight>
-};
-
-const getNextId = () => String(Math.random()).slice(2);
-
-const parseIdFromHash = () => window.location.hash.slice("#highlight-".length);
-
-const resetHash = () => {
-  window.location.hash = "";
-};
-
-const HighlightPopup = ({ comment }) =>
-  comment.text ? (
-    <div className="Highlight__popup">
-      {comment.emoji} {comment.text}
+      <Route
+        exact path={routes.LANDING}
+        component={() => <LandingPage />}
+      />
+      <Route
+        exact path={routes.SIGN_UP}
+        component={() => <SignUpPage />}
+      />
+      <Route
+        exact path={routes.SIGN_IN}
+        component={() => <SignInPage />}
+      />
+      <Route
+        exact path={routes.PASSWORD_FORGET}
+        component={() => <PasswordForgetPage />}
+      />
+      <Route
+        exact path={routes.HOME}
+        component={() => <HomePage />}
+      />
+      <Route
+        exact path={routes.ACCOUNT}
+        component={() => <AccountPage />}
+      />
     </div>
-  ) : null;
-
-// const DEFAULT_URL = 'https://arxiv.org/pdf/1203.0057.pdf'; 
-
-const papers = [
-    "coner.pdf",
-    "conf_icwsm_BandariAH12.pdf",
-    "conf_trec_BellotCEGL02.pdf",
-  ]
-
-const DEFAULT_URL = `pdf/${papers[2]}`;
-
-const searchParams = new URLSearchParams(window.location.search);
-const url = searchParams.get("url") || DEFAULT_URL;
-
-class App extends Component<Props, State> {
-  state = {
-    highlights: termHighlights[url.split("/")[1]] ? [...termHighlights[url.split("/")[1]]] : []
-  };
-
-  state: State;
-
-  resetHighlights = () => {
-    this.setState({
-      highlights: []
-    });
-  };
-
-  scrollViewerTo = (highlight: any) => {};
-
-  scrollToHighlightFromHash = () => {
-    const highlight = this.getHighlightById(parseIdFromHash());
-
-    if (highlight) {
-      this.scrollViewerTo(highlight);
-    }
-  };
-
-  componentDidMount() {
-    window.addEventListener(
-      "hashchange",
-      this.scrollToHighlightFromHash,
-      false
-    );
-  }
-
-  getHighlightById(id: string) {
-    const { highlights } = this.state;
-
-    return highlights.find(highlight => highlight.id === id);
-  }
-
-  addHighlight(highlight: T_NewHighlight) {
-    const { highlights } = this.state;
-
-    console.log("Saving highlight", highlight);
-
-    this.setState({
-      highlights: [{ ...highlight, id: getNextId() }, ...highlights]
-    });
-  }
-
-  updateHighlight(highlightId: string, position: Object, content: Object) {
-    console.log("Updating highlight", highlightId, position, content);
-
-    this.setState({
-      highlights: this.state.highlights.map(h => {
-        return h.id === highlightId
-          ? {
-              ...h,
-              position: { ...h.position, ...position },
-              content: { ...h.content, ...content }
-            }
-          : h;
-      })
-    });
-  }
-
-  render() {
-    const { highlights } = this.state;
-
-    return (
-      <div className="App" style={{ display: "flex", height: "100vh" }}>
-        <Sidebar
-          highlights={highlights}
-          resetHighlights={this.resetHighlights}
-        />
-        <div
-          style={{
-            height: "100vh",
-            width: "75vw",
-            overflowY: "scroll",
-            position: "relative"
-          }}
-        >
-          <PdfLoader url={url} beforeLoad={<Spinner />}>
-            {pdfDocument => (
-              <PdfAnnotator
-                pdfDocument={pdfDocument}
-                enableAreaSelection={event => event.altKey}
-                onScrollChange={resetHash}
-                scrollRef={scrollTo => {
-                  this.scrollViewerTo = scrollTo;
-
-                  this.scrollToHighlightFromHash();
-                }}
-                url={url}
-                onSelectionFinished={(
-                  position,
-                  content,
-                  hideTipAndSelection,
-                  transformSelection
-                ) => (
-                  <Tip
-                    onOpen={transformSelection}
-                    onConfirm={comment => {
-                      console.log(content, position, comment)
-                      this.addHighlight({ content, position, comment });
-
-                      hideTipAndSelection();
-                    }}
-                  />
-                )}
-                highlightTransform={(
-                  highlight,
-                  index,
-                  setTip,
-                  hideTip,
-                  viewportToScaled,
-                  screenshot,
-                  isScrolledTo,
-                  renderTipAtPosition
-                ) => {
-                  const isTextHighlight = !Boolean(
-                    highlight.content && highlight.content.image
-                  );
-
-                  const component = isTextHighlight ? (
-                    <Highlight
-                      isScrolledTo={isScrolledTo}
-                      position={highlight.position}
-                      comment={highlight.comment}
-                      type={highlight.type}
-                      onClick={() => {
-                        // setTip(highlight, highlight => "yo")
-                        }
-                      }
-                    />
-                  ) : (
-                    <AreaHighlight
-                      highlight={highlight}
-                      onChange={boundingRect => {
-                        this.updateHighlight(
-                          highlight.id,
-                          { boundingRect: viewportToScaled(boundingRect) },
-                          { image: screenshot(boundingRect) }
-                        );
-                      }}
-                    />
-                  );
-
-                  return (
-                    <Popup
-                      popupContent={<HighlightPopup {...highlight} />}
-                      onMouseOver={popupContent =>
-                        setTip(highlight, highlight => popupContent)
-                      }
-                      onMouseOut={hideTip}
-                      key={index}
-                      children={component}
-                    />
-                  );
-                }}
-                highlights={highlights}
-              />
-            )}
-          </PdfLoader>
-        </div>
-      </div>
-    );
-  }
-}
+  </Router>
 
 export default App;
-
