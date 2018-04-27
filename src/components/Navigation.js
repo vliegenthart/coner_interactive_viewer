eceeaa328d6d1042e293e629f06de4a80765964e
Vliegenthart <daniel.vliegenthart@gmail.com>
2018-04-27 21:31:11 +0900

    Add basic sign in, signout and conditional rendering for authenticed and non authenticated users

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+
+import SignOutButton from './SignOut';
 import * as routes from '../constants/routes';
 
-const Navigation = () =>
+const Navigation = ({ authUser }) =>
   <div>
-    <ul>
-      <li><Link to={routes.SIGN_IN}>Sign In</Link></li>
-      <li><Link to={routes.LANDING}>Landing</Link></li>
-      <li><Link to={routes.HOME}>Viewer</Link></li>
-      <li><Link to={routes.ACCOUNT}>Account</Link></li>
-    </ul>
+    { authUser
+      ? <NavigationAuth />
+      : <NavigationNonAuth />
+    }
   </div>
+
+const NavigationAuth = () =>
+  <ul>
+    <li><Link to={routes.LANDING}>Landing</Link></li>
+    <li><Link to={routes.HOME}>Viewer</Link></li>
+    <li><Link to={routes.ACCOUNT}>Account</Link></li>
+    <li><SignOutButton /></li>
+  </ul>
+
+const NavigationNonAuth = () =>
+  <ul>
+    <li><Link to={routes.LANDING}>Landing</Link></li>
+    <li><Link to={routes.SIGN_IN}>Sign In</Link></li>
+  </ul>
+
 export default Navigation;
