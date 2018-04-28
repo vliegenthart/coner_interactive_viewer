b7f7728143c19049331fd884cd4994d1d00c1d3c
Vliegenthart <daniel.vliegenthart@gmail.com>
2018-04-28 16:10:42 +0900

    Implement page authorization

--- src/components/Account.js
+++ src/components/Account.js
@@ -3,6 +3,7 @@ import React from 'react';
 import AuthUserContext from './AuthUserContext';
 import { PasswordForgetForm } from './PasswordForget';
 import PasswordChangeForm from './PasswordChange';
+import withAuthorization from './withAuthorization';
 
 const AccountPage = () =>
   <AuthUserContext.Consumer>
@@ -15,4 +16,7 @@ const AccountPage = () =>
     }
   </AuthUserContext.Consumer>
 
-export default AccountPage;
+
+const authCondition = (authUser) => !!authUser;
+
+export default withAuthorization(authCondition)(AccountPage);
