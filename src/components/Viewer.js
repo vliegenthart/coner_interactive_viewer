16715ba308e20e8863e33c08e1c013978df1f561
Vliegenthart <daniel.vliegenthart@gmail.com>
2018-05-05 21:31:05 +0900

    Sort papers in dropdown on title

--- src/components/Viewer.js
+++ src/components/Viewer.js
@@ -107,7 +107,6 @@ class PdfViewer extends Component<Props, State> {
 
   // Load highlights from firebase database
   getHighlightsByPid(pid: string) {
-    console.log(pid)
     db.onceGetHighlights(pid)
     .then((snapshot) => {
       const highlights = snapshot.val() ? snapshotToArray(snapshot.val()) : []
