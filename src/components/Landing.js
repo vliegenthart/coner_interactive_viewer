61f1104e4003dbfcf40bc6469409d9b8b977e189
Vliegenthart <daniel.vliegenthart@gmail.com>
2018-05-11 13:39:17 +0900

    Update styling for landing, signin and passwordforget page

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -2,10 +2,19 @@ import React from 'react';
 import * as routes from '../constants/routes';
 import { Link } from 'react-router-dom';
 
+import Button from 'material-ui/Button';
+import Paper from 'material-ui/Paper';
+import Grid from 'material-ui/Grid';
+
 const LandingPage = () =>
-  <div>
-    <h1>Coner Interactive Document Viewer</h1>
-    <Link to={routes.VIEWER}>Go to Coner Viewer</Link>
-  </div>
+
+  <Grid container spacing={24} alignItems="center" direction="row" justify="center">
+    <Grid item xs={8}>
+      <Paper className="Basic__paper">
+        <h1>Coner Interactive Document Viewer</h1>
+        <Link to={routes.VIEWER}>Go to Coner Viewer</Link>
+      </Paper>
+    </Grid>
+  </Grid>
 
 export default LandingPage;
