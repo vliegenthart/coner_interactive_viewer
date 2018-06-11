a958a029484e338c851082cf904558e48c993a44
Vliegenthart <daniel.vliegenthart@gmail.com>
2018-06-11 13:03:42 +0100

    Add demo highlight files

---
+++ src/highlights/demo-papers-list.js
@@ -0,0 +1,10 @@
+// @flow 
+
+const papersList = [
+  {
+    "pid": "conf_vldb_WeberSB98",
+    "title": "A quantitative analysis and performance study for similarity-search methods in high-dimensional spaces"
+  }
+];
+
+export default papersList;
---
+++ src/highlights/demo-term-highlights.js
@@ -0,0 +1,232 @@
+// @flow 
+
+const termHighlights = [
+  {
+    "content": {
+      "text": "similarity search"
+    },
+    "position": {
+      "pageNumber": 1,
+      "boundingRect": {
+        "x1": 142,
+        "x2": 243,
+        "y1": 594,
+        "y2": 609,
+        "width": 856,
+        "height": 1110
+      },
+      "rects": [
+        {
+          "x1": 142,
+          "x2": 243,
+          "y1": 594,
+          "y2": 609,
+          "width": 856,
+          "height": 1110
+        }
+      ]
+    },
+    "metadata": {
+      "text": "",
+      "facet": "dataset",
+      "type": "generated",
+      "timestamp": 1526408025
+    },
+    "id": "4431229168",
+    "pid": "conf_vldb_WeberSB98",
+    "title": "A quantitative analysis and performance study for similarity-search methods in high-dimensional spaces"
+  },
+  {
+    "content": {
+      "text": "nearest neighbor"
+    },
+    "position": {
+      "pageNumber": 1,
+      "boundingRect": {
+        "x1": 444,
+        "x2": 770,
+        "y1": 566,
+        "y2": 597,
+        "width": 856,
+        "height": 1110
+      },
+      "rects": [
+        {
+          "x1": 726,
+          "x2": 770,
+          "y1": 566,
+          "y2": 581,
+          "width": 856,
+          "height": 1110
+        },
+        {
+          "x1": 444,
+          "x2": 494,
+          "y1": 582,
+          "y2": 597,
+          "width": 856,
+          "height": 1110
+        }
+      ]
+    },
+    "metadata": {
+      "text": "",
+      "facet": "dataset",
+      "type": "generated",
+      "timestamp": 1526408025
+    },
+    "id": "4431230344",
+    "pid": "conf_vldb_WeberSB98",
+    "title": "A quantitative analysis and performance study for similarity-search methods in high-dimensional spaces"
+  },
+  {
+    "content": {
+      "text": "large data"
+    },
+    "position": {
+      "pageNumber": 1,
+      "boundingRect": {
+        "x1": 336,
+        "x2": 391,
+        "y1": 938,
+        "y2": 950,
+        "width": 856,
+        "height": 1110
+      },
+      "rects": [
+        {
+          "x1": 336,
+          "x2": 391,
+          "y1": 938,
+          "y2": 950,
+          "width": 856,
+          "height": 1110
+        }
+      ]
+    },
+    "metadata": {
+      "text": "",
+      "facet": "dataset",
+      "type": "generated",
+      "timestamp": 1526408025
+    },
+    "id": "4431231688",
+    "pid": "conf_vldb_WeberSB98",
+    "title": "A quantitative analysis and performance study for similarity-search methods in high-dimensional spaces"
+  },
+  {
+    "content": {
+      "text": "high dimensionality"
+    },
+    "position": {
+      "pageNumber": 1,
+      "boundingRect": {
+        "x1": 187,
+        "x2": 298,
+        "y1": 626,
+        "y2": 641,
+        "width": 856,
+        "height": 1110
+      },
+      "rects": [
+        {
+          "x1": 187,
+          "x2": 298,
+          "y1": 626,
+          "y2": 641,
+          "width": 856,
+          "height": 1110
+        }
+      ]
+    },
+    "metadata": {
+      "text": "",
+      "facet": "dataset",
+      "type": "generated",
+      "timestamp": 1526408025
+    },
+    "id": "4431232024",
+    "pid": "conf_vldb_WeberSB98",
+    "title": "A quantitative analysis and performance study for similarity-search methods in high-dimensional spaces"
+  },
+  {
+    "content": {
+      "text": "vldb"
+    },
+    "position": {
+      "pageNumber": 1,
+      "boundingRect": {
+        "x1": 276,
+        "x2": 305,
+        "y1": 913,
+        "y2": 925,
+        "width": 856,
+        "height": 1110
+      },
+      "rects": [
+        {
+          "x1": 276,
+          "x2": 305,
+          "y1": 913,
+          "y2": 925,
+          "width": 856,
+          "height": 1110
+        }
+      ]
+    },
+    "metadata": {
+      "text": "",
+      "facet": "dataset",
+      "type": "generated",
+      "timestamp": 1526408025
+    },
+    "id": "4421394384",
+    "pid": "conf_vldb_WeberSB98",
+    "title": "A quantitative analysis and performance study for similarity-search methods in high-dimensional spaces"
+  },
+  {
+    "content": {
+      "text": "decision support"
+    },
+    "position": {
+      "pageNumber": 1,
+      "boundingRect": {
+        "x1": 135,
+        "x2": 422,
+        "y1": 843,
+        "y2": 873,
+        "width": 856,
+        "height": 1110
+      },
+      "rects": [
+        {
+          "x1": 401,
+          "x2": 422,
+          "y1": 843,
+          "y2": 857,
+          "width": 856,
+          "height": 1110
+        },
+        {
+          "x1": 135,
+          "x2": 179,
+          "y1": 859,
+          "y2": 873,
+          "width": 856,
+          "height": 1110
+        }
+      ]
+    },
+    "metadata": {
+      "text": "",
+      "facet": "dataset",
+      "type": "generated",
+      "timestamp": 1526408025
+    },
+    "id": "4421415488",
+    "pid": "conf_vldb_WeberSB98",
+    "title": "A quantitative analysis and performance study for similarity-search methods in high-dimensional spaces"
+  }
+];
+
+export default termHighlights;
